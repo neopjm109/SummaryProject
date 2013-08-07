f06e108e5ef9e6cd05441f8131000b10dea64e56
neopjm109 <neopjm109@gmail.com>
2013-08-07 19:31:51 +0900

    130807 J.M.Park You can text sizing

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -11,26 +11,29 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	Button btnSummary, btnLoad;
+	Button btnSummary, btnLoad, btnTextSizeUp, btnTextSizeDown;
 	TextView mtext;
 	CharSequence[] items = {"탐색기","웹페이지"};
 	private String filePath;
 	
 	String beforeSummary, afterSummary = "요약하기!";
 	boolean btnSummaryOn = false;
+	
+	int textSizeCount = 0;
 
 
 	@Override
@@ -39,66 +42,112 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator;
-		
-		btnSummary = (Button) findViewById(R.id.btnSummary);
+
+		btnSummary = (Button)findViewById(R.id.btnSummary);
 		btnLoad = (Button)findViewById(R.id.btnLoad);
 		mtext = (TextView)findViewById(R.id.mtext);
 		btnSummary.setOnClickListener(listener);
 		btnLoad.setOnClickListener(listener);
 
+		
+		LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout upLayer = (LinearLayout)inflater.inflate(R.layout.up_layer, null);
+		
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+		addContentView(upLayer, params);
+
+		btnTextSizeUp = (Button)upLayer.findViewById(R.id.btnTextSizeUp);
+		btnTextSizeDown = (Button)upLayer.findViewById(R.id.btnTextSizeDown);
+		
+		btnTextSizeUp.setOnClickListener(listener);
+		btnTextSizeDown.setOnClickListener(listener);
 	}
 	View.OnClickListener listener = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
-			  if (v.getId() == R.id.btnSummary){
-				  // 요약하기
-				  if(btnSummaryOn){
-					  mtext.setText(beforeSummary);
-					  btnSummary.setText("요약하기");
-				  }
-				  else{
-					  WSummary.SummaryCheck(afterSummary);
-					  mtext.setText(afterSummary);
-					  btnSummary.setText("원문보기");
-				  }
-				  btnSummaryOn = !btnSummaryOn;
+			// 글씨 확대 축소
+			if(v.getId() == R.id.btnTextSizeUp){
+				if(textSizeCount < 3){
+					switch(textSizeCount){
+					case 0:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 35);
+						break;
+					case 1:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50);
+						break;
+					case 2:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 75);
+						break;
+					default:
+						break;
+					}
+					textSizeCount++;
+				}
+			}else if(v.getId() == R.id.btnTextSizeDown){
+				if(textSizeCount > 0){	
+					switch(textSizeCount){
+					case 1:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+						break;
+					case 2:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 35);
+						break;
+					case 3:
+						mtext.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50);
+						break;
+					default:
+						break;
+					}
+					textSizeCount--;
+				}
+			}
+			// 주 메뉴
+			if (v.getId() == R.id.btnSummary){
+				// 요약하기
+				if(btnSummaryOn){
+					mtext.setText(beforeSummary);
+					btnSummary.setText("요약하기");
+				}
+				else{
+					WSummary.SummaryCheck(afterSummary);
+					mtext.setText(afterSummary);
+					btnSummary.setText("원문보기");
+				}
+				btnSummaryOn = !btnSummaryOn;
 				  
-			  }else if(v.getId() == R.id.btnLoad){
-				  new AlertDialog.Builder(MainActivity.this).setTitle("선택하세요")
-				  .setItems(items,new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int item) {
-						// TODO Auto-generated method stub
-						 if(items[item] == "탐색기")
-							 onTXTRead();
-						 if(items[item] == "웹페이지"){
-							 LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-							 View layout = inflater.inflate(R.layout.html_dialog, (ViewGroup)findViewById(R.id.htmlLayout));
-							 final EditText htmlEdit = (EditText)layout.findViewById(R.id.htmlEdit);
-							 
-							 new AlertDialog.Builder(MainActivity.this).setTitle("웹페이지")
-							 .setView(layout).setPositiveButton("확인", new DialogInterface.OnClickListener(){
-
-								@Override
-								public void onClick(DialogInterface dialog,
-										int which) {
-									// TODO Auto-generated method stub
-									onHtmlRead(htmlEdit.getText().toString());
-								}
-								 
-							 }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
-								
+			}
+			if(v.getId() == R.id.btnLoad){
+				new AlertDialog.Builder(MainActivity.this).setTitle("선택하세요")
+				.setItems(items,new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int item) {
+					// TODO Auto-generated method stub
+					if(items[item] == "탐색기")
+						onTXTRead();
+						if(items[item] == "웹페이지"){
+							LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+							LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.html_dialog, null);
+							final EditText htmlEdit = (EditText)layout.findViewById(R.id.htmlEdit);
+							
+							new AlertDialog.Builder(MainActivity.this).setTitle("웹페이지")
+							.setView(layout).setPositiveButton("확인", new DialogInterface.OnClickListener(){
 								@Override
 								public void onClick(DialogInterface dialog, int which) {
 									// TODO Auto-generated method stub
-									
+									onHtmlRead(htmlEdit.getText().toString());
 								}
+							}).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									// TODO Auto-generated method stub
+									}
 							}).show();
 						 }
 					}
 				}).show();
-			  }
+			}
 		}
 	};
 	
