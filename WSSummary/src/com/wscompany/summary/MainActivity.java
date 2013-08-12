f9b0e8ed88e6fbf7c9204fa20dfe784158a507ff
neopjm109 <neopjm109@gmail.com>
2013-08-12 22:58:33 +0900

    130812 JMPark Add base in a WHtmlParser

--- WSSummary/src/com/wscompany/summary/MainActivity.java
+++ WSSummary/src/com/wscompany/summary/MainActivity.java
@@ -1,11 +1,17 @@
 package com.wscompany.summary;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	//
+	private String beforeSummary = "", afterSummary ="";
+	DownThread mThread;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +24,31 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
 
+	class DownThread extends Thread{
+		String mAddr;
+		String mResult;
+		
+		DownThread(String addr){
+			mAddr = addr;
+			mResult = "";
+		}
+		
+		public void run(){
+			mResult = WHtmlParser.DownloadHtml(mAddr);
+			mAfterDown.sendEmptyMessage(0);
+		}		
+		Handler mAfterDown = new Handler(){
+			public void handleMessage(Message msg) {
+				
+				beforeSummary = mThread.mResult;
+				/*
+				beforeSummary = WHtmlConverter.OrderSentence(mAddr, mResult);
+	    		mtext.setText(beforeSummary);
+	    		*/
+//		    		mProgress.dismiss();
+	    	}
+		};
+	}
 }
--- WSSummary/src/com/wscompany/summary/WHtmlParser.java
+++ WSSummary/src/com/wscompany/summary/WHtmlParser.java
@@ -1,5 +1,46 @@
 package com.wscompany.summary;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Pattern;
+
 public class WHtmlParser {
 
+	Pattern script, css, html;
+	
+	static String source, mAddr;
+	static StringBuffer content;
+	
+	public static String DownloadHtml(String addr){
+		mAddr = addr;
+		content = new StringBuffer();
+
+		try{
+			URL url = new URL(mAddr);
+			InputStream is = url.openStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+
+			String inStr = "";
+			while((inStr = br.readLine()) != null){
+				content.append(inStr + "\n");
+			}
+			
+		}catch(Exception e){
+			
+		}
+		
+		source = new String(content);
+		return source;
+	}
+	
+	public static void ChangeBr(String source){
+		
+	}
+	
+	public static void RemoveTag(String source){
+		
+	}
 }
