ee26474098721fc1a5a4dc05f27b7b655dd891df
Kimyongjin <yongjin5184@gmail.com>
2013-08-14 16:19:55 +0900

    130814 yongjin sliding_design

--- WSSummary/gen/com/wscompany/summary/R.java
+++ WSSummary/gen/com/wscompany/summary/R.java
@@ -22,12 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int summary_logo=0x7f020001;
+        public static final int summary_logo2=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int BtnSlide=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int app=0x7f080000;
+        public static final int list=0x7f080004;
+        public static final int menu=0x7f080003;
+        public static final int tabBar=0x7f080001;
+        public static final int top=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int horz_scroll_app=0x7f030000;
+        public static final int horz_scroll_menu=0x7f030001;
+        public static final int horz_scroll_with_list_menu=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
